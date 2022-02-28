b4f215e9de9215cd3ce3890155e02853bfd263bb
XI3386-kushagraw <99240268+XI3386-kushagraw@users.noreply.github.com>
2022-02-28 14:09:03 +0530

    Operators

---
+++ src/com/xebia/bcp7/operators/PrenPost.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.xebia.bcp7.operators;
+
+/**
+ * @author Kushagra.Wadhwa
+ *
+ */
+public class PrenPost {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=0,j=0;
+		System.out.println(i);
+		System.out.println(j);
+	}
+
+}
